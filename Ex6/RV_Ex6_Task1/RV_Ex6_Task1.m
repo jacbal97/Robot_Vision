d7f8b53d973d52a90464c1f1304c47b1fe42b22e
jacbal97 <jacbal97@wp.pl>
2021-11-22 21:17:25 +0100

    Added first task from Ex6

---
+++ Ex6/RV_Ex6_Task1/RV_Ex6_Task1.m
@@ -0,0 +1,35 @@
+% Clear Works
+clear;
+clc;
+close all;
+
+% Load Images
+img_1 = imread('..\images_1\L_1.bmp');
+img_2 = imread('..\images_1\L_2.bmp');
+img_3 = imread('..\images_1\L_3.bmp');
+img_4 = imread('..\images_1\L_4.bmp');
+img_5 = imread('..\images_1\L_5.bmp');
+
+% Show Result
+show(img_1);
+show(img_2);
+show(img_3);
+show(img_4);
+show(img_5);
+
+% Functions
+function x = getArea(I)
+    x = sum(sum(I));
+end
+
+function [size, img] = getPerimeter(I)
+    img = I - imerode(I, strel('square', 3));
+    size = sum(sum(img));
+end
+
+function show(I)
+    [per, img] = getPerimeter(I);
+    fprintf("Area: %d,\tPerimeter: %d\n", getArea(I), per);
+    figure;
+    imshow(img);
+end
---
+++ Ex6/RV_Ex6_Task2/RV_Ex6_Task2.m
@@ -0,0 +1,11 @@
+% Clear Works
+clear;
+clc;
+close all;
+
+% Load Images
+img_1 = imread('..\images_1\L_1.bmp');
+img_2 = imread('..\images_1\L_2.bmp');
+img_3 = imread('..\images_1\L_3.bmp');
+img_4 = imread('..\images_1\L_4.bmp');
+img_5 = imread('..\images_1\L_5.bmp');
